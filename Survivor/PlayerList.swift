a61e8cff4ca78559a5362d9b9ad62f4c987c44bf
Pat McGuire <patmcguire0890@gmail.com>
2015-06-02 22:05:00 -0500

    Adding the UI for the PlayerList screen.

---
+++ Survivor/PlayerList.swift
@@ -0,0 +1,18 @@
+//
+//  PlayerList.swift
+//  Survivor
+//
+//  Created by Pat McGuire on 6/2/15.
+//  Copyright (c) 2015 spark. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Parse
+
+class PlayerList: UIViewController{
+    
+    
+    
+    
+}
